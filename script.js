0e9d974168729561d4d5e405c07c7ddeac41cee0
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2023-03-24 15:33:53 -0300

    feat/ criação dos metodos de arrastar os elementos da tela

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 document.querySelectorAll('.item').forEach((item) => {
   item.addEventListener('dragstart', dragStart);
   item.addEventListener('dragend', dragEnd);
+  item.addEventListener('dragover', dragOver);
+  item.addEventListener('dragleave', dragLeave);
+  item.addEventListener('drop', drop);
+});
+
+document.querySelectorAll('.area').forEach(area => {
+  area.addEventListener('dragover', dragOver);
+  area.addEventListener('dragleave', dragLeave);
+  area.addEventListener('drop', drop);
 });
 
 // Functions Item
-
 function dragStart(e) {
-  e.currentTarget.classList.add('dragging');
+  e.currentTarget.classList.add('dragging'); 
 }
 
 function dragEnd(e) {
   e.currentTarget.classList.remove('dragging');
 }
+
+//* Function Area
+function dragOver(e){
+  e.preventDefault();
+  e.currentTarget.classList.add('hover');  
+};
+
+function dragLeave(e) {
+  e.currentTarget.classList.remove('hover');
+};
+
+function drop(e) {
+  e.currentTarget.classList.remove('hover');
+  let dragItem = document.querySelector('.item.dragging');
+  
+  if(e.currentTarget.querySelector('.item') === null) {
+     e.currentTarget.appendChild(dragItem);
+  };
+};
